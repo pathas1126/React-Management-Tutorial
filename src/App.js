bad414bc733ffada0f4bae916b852dc687264277
pathas <pathas@naver.com>
2020-01-27 13:52:48 +0900

    컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -2,26 +2,56 @@ import React from "react";
 import Customer from "./components/Customer";
 import "./App.css";
 
-const customer = {
-  id: 1,
-  image: "https://placeimg.com/64/64/any",
-  name: "홍길동",
-  birthday: "961222",
-  gender: "남자",
-  job: "대학생"
-};
+// 고객 데이터
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/0",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "이순신",
+    birthday: "921222",
+    gender: "남자",
+    job: "프로그래머"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "정약용",
+    birthday: "911222",
+    gender: "남자",
+    job: "대학원생"
+  }
+];
 
 class App extends React.Component {
   render() {
     return (
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {/* customers가 배열이기 때문에 map 메소드를 이용해서 소스코드 절약 가능
+        각각의 원소를 c라는 매개변수로 해서 원소마다 가지고 있는 데이터를 출력 */}
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              /* 일반적으로 map을 사용할 때는 key라는 속성을 설정해 주어야 함
+                id값을 1부터 설정했기 때문에 여기서는 key값을 c.id를 넣어줌 */
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -6,7 +6,43 @@ class Customer extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
+        <CustomerProfile
+          id={this.props.id}
+          image={this.props.image}
+          name={this.props.name}
+        ></CustomerProfile>
+        <CustomerInfo
+          birthday={this.props.birthday}
+          gender={this.props.gender}
+          job={this.props.job}
+        ></CustomerInfo>
+      </div>
+    );
+  }
+}
+
+// ==============컴포넌트 분리 ==================
+/* 컴포넌트 분리는 유지보수에 용이함 */
+
+// 사용자 프로필 이미지, 이름, 아이디 정보
+class CustomerProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.image} alt="profile" />
+        <h2>
+          {this.props.name}({this.props.id})
+        </h2>
+      </div>
+    );
+  }
+}
+
+// 사용자의 남은 정보
+class CustomerInfo extends React.Component {
+  render() {
+    return (
+      <div>
         <p>{this.props.birthday}</p>
         <p>{this.props.gender}</p>
         <p>{this.props.job}</p>
